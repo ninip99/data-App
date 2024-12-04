2b3a492ef2be05df62b64561034656b96de041d2
ninip99 <ninip1004@gmail.com>
2024-12-04 17:30:15 +0900

    Add files via upload

--- home.py
+++ home.py
@@ -18,6 +18,6 @@ st.markdown(
     "아이리스 품종 중 Setosa, Versicolor, Virginica 분류에 대한"
     "  로널드 피셔의 1936년 논문에서 사용된 데이터 셋"
     "붓꽃 iris는 3가지 종류 Setosa, Versicolor, Virginica를 가지고 있다."
-    "각 데이터 포인트는 꽃받침(sepal)과 꽃잎(petal)의 길이와 너비를 
+    "각 데이터 포인트는 꽃받침(sepal)과 꽃잎(petal)의 길이와 너비를 "
     "        나타내는 4개의 특성을 가지고 있습니다."
             )
